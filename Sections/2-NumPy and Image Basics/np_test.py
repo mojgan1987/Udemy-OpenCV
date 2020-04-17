11be67bee3920e10c57cb91d8852cd0baec4b24f
Mojgan Hashemian <mojgan1987@gmail.com>
2020-04-17 18:22:59 +0100

    Update np_test.py

--- Sections/2-NumPy and Image Basics/np_test.py
+++ Sections/2-NumPy and Image Basics/np_test.py
@@ -8,3 +8,28 @@ type(myarray)
 # numpy.ndarray
 
 np.arange(1,10)
+np.zeros(shape=(10,5))
+np.ones((2,5))
+
+np.random.seed(101)
+arr = np.random.randint(0,100,10)
+arr
+
+arr.max()
+arr.argmax()
+
+arr.shape
+arr.reshape((2,5))
+
+mat= np.arange(0,100).reshape(10,10)
+mat
+row = 0
+col = 1
+mat[row, col]
+mat[:,1]
+
+mat[:,1].reshape(1,10)
+mat[0:3,0:3]
+
+
+
