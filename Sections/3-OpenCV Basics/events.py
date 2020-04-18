ee7a29e5350130763dc1b7028361991f12890e87
Mojgan Hashemian <mojgan1987@gmail.com>
2020-04-18 19:51:54 +0100

    Create events.py

---
+++ Sections/3-OpenCV Basics/events.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+#########################
+######## Function #######
+#########################
+
+def draw_circle(event,x,y,flags,pram):
+    
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x,y),100, (0,255,0), -1)
+
+cv2.namedWindow(winname='my_drawing')
+
+cv2.setMouseCallback('my_drawing',draw_circle)
+
+
+######################################
+##### Showing Image with OpenCV ######
+######################################
+
+img = np.zeros((512,512,3),np.int8) #np.int8 gray
+
+while True:
+    cv2.imshow('my_drawing', img)
+    
+    if cv2.waitKey(20) & 0xFF ==27:
+        break
+        
+cv2.destroyAllWindows()
