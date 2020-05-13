b1015be98c2894fb5ab7382172ddf9e488ffb46a
Mojgan Hashemian <mojgan1987@gmail.com>
2020-05-13 22:46:19 +0100

    Create MeanSHift_CAMShift.py

---
+++ Sections/7-Object Tracking/MeanSHift_CAMShift.py
@@ -0,0 +1,7 @@
+# weighted average
+# points move to center of centroid
+# when points stopped moveing, we are at the center
+# not always the best clusters
+
+# calculate the color histogram of the target aeaand keep sliding the tracking window to the closest match
+# if the target gets a way or moves toward the camera, mean shift does not work -> CAM (continousy adapted meanshift)
