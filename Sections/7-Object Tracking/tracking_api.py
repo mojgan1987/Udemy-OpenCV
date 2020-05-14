81b7871399681e23d99bf167bf4e163274a7cb45
Mojgan Hashemian <mojgan1987@gmail.com>
2020-05-14 20:15:18 +0100

    Create tracking_api.py

---
+++ Sections/7-Object Tracking/tracking_api.py
@@ -0,0 +1,22 @@
+# boosting tracker
+# pros: well studied algo
+# cons: does not know if it fails
+
+# mil tracker
+# neighborhood
+# pros: good performance
+# cons: does not know if it fails can't recover 
+
+# KCF 
+# kernalized: overlapping data points
+# pros: perform better
+# cons: cannot recover
+
+# TLD
+# good under large changes
+# cons: many false positives
+
+# median flow:
+# very good at reporting failed tracking, and predictable lotions
+# cons: fails under large motion
+
